68fb183b61c9325bd1bceb9cbfc98c4f988ea6df
Dipti <shipate1998@gmail.com>
2021-08-21 11:07:08 +0530

    committing project

---
+++ Testing/src/src/test1/FlipkartTest.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package src.test1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ *
+ * @author DILIP
+ */
+public class FlipkartTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        // TODO code application logic here
+       System.setProperty("webdriver.chrome.driver","C:\\Users\\DILIP\\Desktop\\sel\\chromedriver_win32\\chromedriver.exe");
+          WebDriver driver = new ChromeDriver();
+          driver.get("http://www.amazon.com/");
+          driver.findElement(By.id("twotabsearchtextbox")).sendKeys("Dell");
+         driver.findElement(By.xpath("//input[@value='Search']")).click();
+          
+          
+          
+          
+    }
+    
+}
---
+++ Testing/src/src/test1/Test_video.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package src.test1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ *
+ * @author DILIP
+ */
+public class Test_video {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws InterruptedException {
+        // TODO code application logic here
+        
+        String regex = "^[\\w-_\\.+]*[\\w-_\\.]\\@([\\w]+\\.)+[\\w]+[\\w]$";
+        String pass="^[0-9]";
+      //return email.matches(regex);
+        System.setProperty("webdriver.chrome.driver","F:\\netbeans-projects\\Test1\\chromedriver_win32 (1)\\chromedriver.exe");
+
+        WebDriver driver=new ChromeDriver();
+        driver.get("file:///C:/Users/DILIP/Desktop/web/videoboot.html");
+        driver.manage().window().maximize();
+        String email="diptishipate@gmail.com";
+        String password="^[0-9]{4}";
+        String p="1234";
+        // test case 1 match the regular expression.
+        if(email.matches(regex)&& p.matches(password))
+        {  
+        driver.findElement(By.id("username")).sendKeys(email);
+          
+            driver.findElement(By.id("pwd")).sendKeys(p);
+                Thread.sleep(2000);
+              driver.findElement(By.id("b1")).click();
+                Thread.sleep(2000);
+              System.out.println("Test case passed");
+            // driver.navigate().forward();
+             driver.findElement(By.id("d1")).click();
+             driver.navigate().back();
+             Thread.sleep(2000);
+             driver.findElement(By.id("d2")).click();
+             driver.navigate().back();
+             Thread.sleep(2000);
+             System.out.println(" The test case 1.3 passed");
+             driver.navigate().back();
+          }
+        
+        
+        else
+        {
+            System.out.print(" test case 1 failed");
+        }
+        
+       
+       // WebDriver d=new ChromeDriver();
+        // d.get("file:///C:/Users/DILIP/Desktop/web/boot-me.html");
+        String g="gmail";
+        Thread.sleep(2000);
+        driver.findElement(By.id("link1")).click();
+        driver.findElement(By.id("identifierId")).sendKeys("shipatedeepti@gmail.com");
+        String t=driver.getTitle();
+        if(g.equalsIgnoreCase(t))
+        {
+            System.out.print("Test case 2 passed");
+        }
+       else
+        {
+            System.out.println(" the test case 2 failed");
+        }
+        driver.navigate().back();
+        Thread.sleep(2000);
+          String f="facebook";
+        driver.findElement(By.id("l")).click();
+        String f1=driver.getTitle();
+        if(f.equalsIgnoreCase(f1))
+        {
+            System.out.println("The test case 3 has passed");
+        }
+        else
+        {
+            System.out.println("\n the test 3 has not passed");
+        }
+        driver.navigate().back();
+        driver.findElement(By.id("regi")).click();
+        driver.findElement(By.id("uid")).sendKeys("ram");
+        driver.findElement(By.id("pwd")).sendKeys("4567");
+        driver.findElement(By.id("aa")).sendKeys("shubham@gmail.com");
+        driver.findElement(By.id("phno")).sendKeys("794034930");
+        Thread.sleep(2000);
+        driver.findElement(By.id("reg")).click();
+        driver.switchTo().alert().accept();
+        System.out.println("\n registration done");
+        driver.get("file:///C:/Users/DILIP/Desktop/web/videoboot.html");
+    }
+   
+}
